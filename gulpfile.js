129c6338bb2b15ba499bb8368d328f5f4c14985e
Vitalii Chalbash <vitaliichalbash@gmail.com>
2022-07-27 14:30:53 +0200

    init gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+import browserSync from 'browser-sync';
+import gulp from 'gulp';
+import autoPrefix from 'gulp-autoprefixer';
+import clean from 'gulp-clean';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCss from 'gulp-clean-css';
+import concat from 'gulp-concat';
+import imageMin from 'gulp-imagemin';
+import minifyjs from 'gulp-js-minify';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import gulpIf from 'gulp-if';
+
+const sass = gulpSass(dartSass);
+browserSync.create()
+
+//Creat paths
+
+const path = {
+	src: {
+		scss: './src/scss/**/*.scss',
+		js: './src/js/**/*.js',
+		img: './src/img/*',
+	},
+	dist: {
+		self: './dist/',
+		css: './dist/css/',
+		js: './dist/js/',
+		img: "./dist/images/",
+	},
+
+	setEnv() {
+		this.isProd = process.argv.includes("--prod")
+		this.isDev = !this.isProd
+	},
+
+}
+
+path.setEnv()
+
+//Create functions-tasks
+
+const createScss = () => {
+	gulp
+		.src(path.src.scss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulpIf(path.isProd, autoPrefix({
+			cascade: false
+		})))
+		.pipe(rename("style.min.css"))
+		.pipe(gulp.dest(path.dist.css))
+		.pipe(browserSync.stream())
+}
+
+const createJS = () => {
+	gulp
+		.src(path.src.js)
+}
+const createImg = () => {
+	gulp
+		.src(path.src.img)
+}
+
+
+const createClean = () => {
+	gulp
+		.src(path.dist.self, {
+			allowEmpty: true
+		}).pipe(clean());
+}
+
+const watcher = () => {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	})
+
+	gulp.watch(path.src.scss, createScss).on('change', browserSync.reload)
+}
